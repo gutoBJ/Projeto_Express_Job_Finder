dcd0aea45a5ccf5cc2cd7754968b8b2532e4784d
Augusto <98566110+gutoBJ@users.noreply.github.com>
2026-05-24 12:48:47 -0300

    Criação do model Job, da rota post e inserção de dados na tabela jobs

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const db = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // db connection
 db.authenticate()
@@ -19,7 +23,9 @@ db.authenticate()
   
 
 // routes
-
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Está funcionando!');
 });
+
+// job routes
+app.use('/jobs', require('./routes/jobs'))
